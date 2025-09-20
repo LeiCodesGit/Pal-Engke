3dbd873a38f31f6d4d683c1610976cb0f705bb7a
Leah Rouse Cruzo <lrcruzo.code@gmail.com>
2025-09-20 21:42:53 +0800

    Changes in index.js and added public and views folder

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from "express";
-
+import path from "path";
+import { fileURLToPath } from 'url';
 const app = express();
 const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//set view engine to EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//public folder for static files
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     //test if server is running

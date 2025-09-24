66657dd79502cb400a05cc342ddc21de995266e7
Leah Rouse Cruzo <lrCruzo.code@gmail.com>
2025-09-24 12:54:58 +0800

    Update routes.js

--- server/routes/auth/routes.js
+++ server/routes/auth/routes.js
@@ -1,3 +1,5 @@
+
+import bycrypt from "bycrpt";
 import express from "express";
 
 const authRouter = express.Router();
@@ -11,5 +13,53 @@ authRouter.get("/login", (req, res) => {
 authRouter.get("/register", (req, res) => {
     res.render("auth/register");
 });
+
+//register new users
+authRouter.post("/register", async (req, res) => {
+    const {
+        userType,
+        firstName,
+        lastName,
+        email,
+        contactNumber,
+        password,
+        age
+    } = req.body;
+
+    try {
+        const existingUser = await User.findOne({email});
+        if (existingUser) {
+            return res.status(409).json({message: "Email already exist"});
+        }
+
+        const hashedPassword = await bycrypt.hash(password, 7);
+
+        const newUser = await User.create({
+            userType,
+            firstName,
+            lastName,
+            email,
+            contactNumber,
+            password: hashedPassword,
+            age
+        });
+
+        //check new generated user:
+        console.log("new user _id:")
+        res.status(201).json({
+            message: "User created sucessfully",
+            userId: newUser._id
+        });
+    }
+    catch (error) {
+        console.error("Registration error:", error);
+        res.status(500).json({
+            message: "Registration Failed",
+            error: error.message
+        });
+    }
+            
+
+}
 
 export default authRouter;
